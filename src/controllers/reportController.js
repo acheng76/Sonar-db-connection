a02b144c68435d591bfa95506ebf55530ae7b926
simapanigrahi <136744782+simapanigrahi@users.noreply.github.com>
2024-07-29 18:51:08 +0530

    Update reportController.js
    added Detaildownload for MMP

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -261,9 +261,60 @@ async function getPerfCoreMMPReportData(req, res) {
   }
 }
 
+async function getPerfReportDetailMMPData(req, res) {
+  try {
+    const connection = await oracledb.getConnection(gonetDBConfig);    
+    // const slaType = 'SLT-PDV';
+    // const startTs = '10/01/2023 00:00:00';
+    // const endTs = '10/31/2023 23:59:59';
+    //  "http://localhost:5000/report/perfReportDetailMMPData?slaType=SLT-PDV&month=03&year=2024"
+
+    const month = req.query.month;
+    const year = req.query.year;
+
+    // const month = 03,04;
+    // const year = 2024;
+
+    const startOfMonth = new Date(year, month - 1, 1, 0, 0, 0);
+    const endOfMonth = new Date(year, month, 0, 23, 59, 59);
+    const formatDate = (date) => {
+      const pad = (num) => (num < 10 ? "0" : "") + num;
+      return `${pad(date.getMonth() + 1)}/${pad(
+        date.getDate()
+      )}/${date.getFullYear()} ${pad(date.getHours())}:${pad(
+        date.getMinutes()
+      )}:${pad(date.getSeconds())}`;
+    };
+    const startTs = formatDate(startOfMonth);    
+    const endTs = formatDate(endOfMonth);
+    const sql = fs
+      .readFileSync("./src/query/perfReportDetailMMPData.sql", "UTF-8")
+      .toString();     
+    const queryResult = await connection.execute(sql, {
+      startTs: startTs,
+      endTs: endTs,
+      slaType: slaType,
+    });
+    
+    const transformedData = queryResult.rows.map((row) => {
+      const rowObject = {};
+      queryResult.metaData.forEach((column, index) => {
+        rowObject[column.name] = row[index];
+      });
+      return rowObject;
+    });
+
+    res.json(transformedData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }  
+}
+
 module.exports = {
   getPerfReportData,
   getAvaiReportData,
   getincidentData,
   getPerfCoreMMPReportData,
+  getPerfReportDetailMMPData,
 };
